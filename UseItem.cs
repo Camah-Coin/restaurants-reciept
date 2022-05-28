3454e444752e5a1a5bcc41bfdebd3ae38638f9ec
CamahCoin <106430225+CamahCoin@users.noreply.github.com>
2022-05-29 00:12:58 +0930

    Update UseItem.cs

--- UseItem.cs
+++ UseItem.cs
@@ -4,28 +4,10 @@ using System.Collections;
 public class UseItem : MonoBehaviour {
 
 
-	public static void Use (string itemName) {
-
-		Debug.Log ("Custom item usage executing for item: " + itemName);
-
-		/* 
-		 * TODO implement your own custom code for what happens when an item is used
-		 * 
-		 * EXAMPLE USAGE:
-		 * 
-		if (itemName == "Flippers") {
-			YourOwnScript.AddToInvetory("Flippers");
-		}
+	public static void Use (string itemName)
+	{
+		//print
 		
-		if (itemName == "Unlock All Vehicles") {
-			YourOwnScript.UnlockVehicles();
-		}
-
-		if (itemName == "Extra Life") {
-			YourOwnScript.addLives(1);
-		}
-		*/
-
 	}
 
 }
