47f494b5b24bb32410195389e434f2a5a6499608
Rieser Stern <RieserStern@gmail.com>
2022-05-28 11:14:10 -0400

    Add code to send text file

--- createRecipt.cs
+++ createRecipt.cs
@@ -22,4 +22,29 @@ public class createRecipt : MonoBehaviour
 
         SceneManager.LoadScene(sceneBuildIndex:1);
     }
+
+    public void uploadText()
+    {
+        // TODO: replace with the URL of Swift webservice
+        string url = "https://www.swiftpos.com.au";
+
+        // TODO: Obtain name and content of the file you want to upload
+        string filename = "hello.text";
+        string content = "Hello World";
+
+        // Create a Web Form
+        WWWForm form = new WWWForm();
+        form.AddField("fileContent", content);
+        form.AddField("fileName", filename);
+        
+        // Upload to a cgi script
+        WWW w = new WWW(url, form);
+        yield return w;
+        if (!string.IsNullOrEmpty(w.error)) {
+            print(w.error);
+        }
+        else {
+            print("Finished Uploading File");
+        }
+    }
 }
